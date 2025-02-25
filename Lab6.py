2b0d6445fc7da468f98f5b0fdcb88a9ee1fd604a
Kendra Hoskin <kendra.hoskin@flemingcollege.ca>
2025-02-25 12:00:48 -0500

    Step 4/5

--- Lab6.py
+++ Lab6.py
@@ -1,6 +1,7 @@
 #Importes
 import requests
 import hashlib
+import pathlib
 
 BASE_URL = "https://download.videolan.org/pub/videolan/vlc/3.0.21/win64/"
 FILE_NAME_SHA256 = "vlc-3.0.21-win64.exe.sha256"
@@ -33,3 +34,18 @@ def download_installer():
 def installer_ok(installer_data, expected_sha256):
     sha256 = hashlib.sha256(installer_data).hexdigest()
     print(f"Computed SHA-256: {sha256}")
+
+#Step 4
+    if sha256 == expected_sha256:
+        print("SHA-256 values match. Integrity verified.")
+        return True
+    else:
+        print("Downloaded SHA-256 does not match expected value. Exiting...")
+        return False
+#Step 5
+def save_installer(installer_data):
+    file_path = pathlib.Path(os.getenv('TEMP')) / FILE_NAME
+    with open(file_path, "wb") as outfile:  # Write a binary file
+        outfile.write(installer_data)
+    print(f"Installer saved to: {file_path}")
+    return file_path
